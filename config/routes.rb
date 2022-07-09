4a8d35e86c6bd54e99a7bc96f717a10461749af2
krkyk <madeinpink1111@gmail.com>
2022-07-09 07:06:48 +0000

    [Add]ヘッダー/フッター

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
